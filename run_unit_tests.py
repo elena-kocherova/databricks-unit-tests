ad43b6b48f7bb1ad9c4dd4b088994cd9b17247f9
elena-kocherova <elena.kocherova@bjss.com>
2025-07-28 13:58:45 +0100

    Update run_unit_tests.py

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -4,19 +4,20 @@
 # COMMAND ----------
 
 import pytest
-import os
+# import os
 import sys
 
 # Run all tests in the repository root.
-notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
-repo_root = "/Workspace/Users/elena.kocherova@nationalhighways.co.uk/unit_tests/" #os.path.dirname(os.path.dirname(notebook_path))
-os.chdir(f'{repo_root}')
-%pwd
+# notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+# repo_root = "/Workspace/Users/elena.kocherova@nationalhighways.co.uk/unit_tests/" #os.path.dirname(os.path.dirname(notebook_path))
+# os.chdir(f'{repo_root}')
+# %pwd
 
 # Skip writing pyc files on a readonly filesystem.
 sys.dont_write_bytecode = True
 
-retcode = pytest.main([".", "-p", "no:cacheprovider"])
+# Run pytest.
+retcode = pytest.main([".", "-v", "-p", "no:cacheprovider"])
 
-# Fail the cell execution if we have any test failures.
-assert retcode == 0, 'The pytest invocation failed. See the log above for details.'
+# Fail the cell execution if there are any test failures.
+assert retcode == 0, "The pytest invocation failed. See the log for details."
