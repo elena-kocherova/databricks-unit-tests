716282c73fbe51614435f512744fda56470e059e
elena-kocherova <elena.kocherova@bjss.com>
2025-09-26 11:28:40 +0100

    Update test_sum_values_function.py

--- test_sum_values_function.py
+++ test_sum_values_function.py
@@ -1,18 +1,22 @@
 import pytest
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame, SparkSession
 from sum_values_function import sum_values
 
 class TestSumValues:
     @pytest.fixture(scope="class")
-    def spark(self):
-        return SparkSession.builder.appName("TestSumValues").getOrCreate()
+    def spark(self) -> SparkSession:
+     return SparkSession.builder.appName("TestSumValues").getOrCreate()
 
     @pytest.fixture(scope="class")
-    def sample_df(self, spark):
+    def sample_df(self, spark: SparkSession) -> DataFrame:
         data = [(3,), (7,), (10,)]
         return spark.createDataFrame(data, ["value"])
-
-    def test_sum_values(self, sample_df):
-        result = sum_values(sample_df)
-        assert result == 20
-
+    
+    def test_sum_values(self, sample_df: DataFrame) -> None:
+        try:
+            result = sum_values(sample_df, column="value")
+            assert result == 20
+        except TypeError as e:
+            pytest.fail(f"TypeError occurred: {e}")
+        except Exception as e:
+            pytest.fail(f"Unexpected exception occurred: {e}")
